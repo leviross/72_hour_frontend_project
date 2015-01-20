21421807d48b67fb2ad544d25d9f1b2e44f87275
leviross <levross@gmail.com>
2015-01-19 21:05:51 -0800

    finalized audio tracks and playback script

--- js/script.js
+++ js/script.js
@@ -9,53 +9,40 @@ $(document).ready(function(){
     });
   }
   var technoSound = new Howl({urls: ["https://clyp.it/3ilizyrr.mp3"]});
-
+  var rockSound = new Howl({urls: ["https://clyp.it/rlcsxlup.mp3"]});
+  var popSound = new Howl({urls: ["https://clyp.it/aceb4s3j.mp3"]});
+  var soundTrackSound = new Howl({urls: ["https://clyp.it/f1lixvmn.mp3"]});
+  var classicalSound = new Howl({urls: ["https://clyp.it/3k5yo3ko.mp3"]});
+  var jazzSound = new Howl({urls: ["https://clyp.it/x2i1gxeg.mp3"]})
 
 
 
 
   //MAIN FUNCTION THAT PLAYS, PAUSES AND STOPS TRACK WITH GENRE MESSAGE
-  var playTrack = function(element, link, message){
+  var playTrack = function(element, message, song){
     $('#'+element).on('click', function(){
-        technoSound.play();
+        song.play();
         newAlert(message);
     });
     $('#pause'+'-'+element).on('click', function(){
-      technoSound.pause();
+      song.pause();
     })
     $('#stop'+'-'+element).on('click', function(){
-      technoSound.stop();
+      song.stop();
     })
     $('#fadeIn'+'-'+element).on('click', function(){
-      technoSound.stop();
+      song.fadeIn(1.0,20000);
+      newAlert(message);
     })
 
   }
-  var pauseTrack = function(element, link){
-    $(element).on('click', function(){
-        var sound = new Howl({
-          urls: [link]
-        }).pause();
-        //newAlert(message);
-    });
-  }
-  var stopTrack = function(element, link){
-    $(element).on('click', function(){
-        var sound = new Howl({
-          urls: [link]
-        }).stop();
-        //newAlert(message);
-    });
-  }
 
-
-  playTrack("techno","https://clyp.it/3ilizyrr.mp3","Techno");
-  pauseTrack("#pause-techno","https://clyp.it/3ilizyrr.mp3");
-  stopTrack("#stop-techno","https://clyp.it/3ilizyrr.mp3");
-
-
-
-
+  playTrack("techno", "Techno", technoSound);
+  playTrack("rock", "Rock", rockSound);
+  playTrack("pop", "Pop", popSound);
+  playTrack("soundtrack", "SoundTrack", soundTrackSound);
+  playTrack("classical", "Classical", classicalSound);
+  playTrack("jazz", "Jazz", jazzSound);
 
 });
 
